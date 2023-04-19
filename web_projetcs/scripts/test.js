098942074d364ef28640f24b141ceab49c6ca1c4
zRosies <gustavo.enrollment@gmail.com>
2023-04-19 13:46:30 -0300

    sasas

---
+++ web_projetcs/scripts/test.js
@@ -0,0 +1,12 @@
+<script src="scripts/app.js"></script>
+
+`Current Date: ${dayName}, ${monthName} ${d.getDate()}, ${year}`;
+
+document.getElementById("#q").innerHTML = ("Welcome to <em>our</em> neighborhood!")
+
+document.getElementById("#temp").value = gettemperature();
+
+const divs = document.querySelectorAll('div');
+
+let filterC= citynames.filter(city => city.charAt(0)=== "C")
+
