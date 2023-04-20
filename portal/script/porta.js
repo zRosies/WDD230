1644e469dd7a4cec8776f2599943a381082a1b28
zRosies <gustavo.enrollment@gmail.com>
2023-04-20 16:58:17 -0300

    changes

--- portal/script/porta.js
+++ portal/script/porta.js
@@ -1,9 +1,13 @@
+// const new_year = new Date();
+// const options = {year:'long'}
+// const year = new_year.toLocaleString('en-Us', options);
 
-const options = {day: 'numeric', month:'numeric', year:'numeric', hour:'numeric', minute:'numeric', second:'numeric'}
 
 const modi = document.lastModified;
+const lastupdate = new Date(modi);  
 const timezone= 'UTC -7';
-const lastupdate = new Date(modi);
+
 const formtdate = lastupdate.toLocaleString('en-Us');
 
 document.getElementById('date').textContent =  formtdate;
+
