f45dcfc848d9f35328eb3465b430048989c258b6
zRosies <gustavo.enrollment@gmail.com>
2023-04-26 20:28:12 -0300

    commit changes

---
+++ lesson2/scripts/design.js
@@ -0,0 +1,12 @@
+const new_year = new Date();
+const year = new_year.getFullYear();
+document.querySelector('#year').textContent = year
+
+const modi = document.lastModified;
+const lastupdate = new Date(modi);  
+const formtdate = lastupdate.toLocaleString('en-Us');
+
+
+
+document.getElementById('date').textContent =  formtdate;
+
