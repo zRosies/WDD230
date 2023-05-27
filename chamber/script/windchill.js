ea9ec4bec3eb49a39c441db3077a5bdc3771d391
zRosies <gustavo.enrollment@gmail.com>
2023-05-27 16:49:35 -0300

    changes

---
+++ chamber/script/windchill.js
@@ -0,0 +1,38 @@
+
+// let weatherimg=document.querySelector("");
+let temperatureInput= document.querySelector("#temperature");
+let windspeedInput= document.querySelector("#windspeed");
+let windchilloutput = document.querySelector("#windchill");
+
+
+function calculateWindChill(temperature, windspeed){
+    let f=0
+if(temperature <= 50 && windspeed > 3){
+    f= 35.74 + (0.6215*temperature) - 35.65*(windspeed**0.16)+((0.4275*temperature)*(windspeed**0.16));
+    f= `${f.toFixed(2)} km/h`
+
+}
+else{
+    f= "N/A"
+}
+return f
+}
+
+temperatureInput.addEventListener("input", function() {
+ 
+    let temperature=parseFloat(temperatureInput.value) ;
+    let windspeed= parseFloat(windspeedInput.value);
+    windchilloutput.innerHTML= calculateWindChill(temperature, windspeed);
+
+});
+  
+windspeedInput.addEventListener("input", function() {
+
+    let temperature=parseFloat(temperatureInput.value) ;
+    let windspeed= parseFloat(windspeedInput.value);
+    windchilloutput.innerHTML=calculateWindChill(temperature, windspeed);
+
+});
+
+
+// windchilloutput.innerHTML=calculateWindChill(temperature,windspeed);
