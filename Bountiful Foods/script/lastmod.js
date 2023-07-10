8c2df21c5c2c1a37012881773e8813085f568eb6
zRosies <gustavo.enrollment@gmail.com>
2023-07-09 21:54:37 -0300

    changes

--- Bountiful Foods/script/lastmod.js
+++ Bountiful Foods/script/lastmod.js
@@ -17,8 +17,51 @@ let opn = { weekday:"long", day:"numeric", month:"long", year:"numeric"};
 const formdate= new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	date
 );
-document.querySelector("#datetime").textContent=formdate;
 
-const hamButton = document.querySelector('#ham');
-const navigation = document.querySelector('.navigation2');
-const closea = document.querySelector("#close");
+//navigation
+// document.querySelector("#datetime").textContent=formdate;
+
+// const hamButton = document.querySelector('#ham');
+
+// const closea = document.querySelector("#close");
+
+
+function toggleMenu(){
+	document.querySelector("nav").classList.toggle("open");
+	// let navigation = document.querySelector("nav")
+    // navigation.classList.toggle("open");
+	console.log("AAA")
+}
+
+const button = document.querySelector('#ham')
+
+button.onclick= toggleMenu;
+
+//fruits
+
+//fruits
+
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+let slideIndex = 1;
+
+if (windowWidth < 667) { 
+  showDivs(slideIndex);
+}
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mypic");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
