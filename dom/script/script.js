3edf19185905bd4376cfc8129d14a65ccb12ebfc
zRosies <gustavo.enrollment@gmail.com>
2023-07-25 22:02:56 -0300

    changes

--- dom/script/script.js
+++ dom/script/script.js
@@ -14,6 +14,16 @@ const button = document.querySelector("button");
 const list = document.querySelector("#list");
 const input= document.querySelector("#favchap");
 
+let points = document.querySelector("#points");
+let completed = document.querySelector("#completed");
+let uncompleted = document.querySelector("#uncompleted");
+let point= 0;
+let complete= 0;
+let uncomplete= 0;
+
+points.innerHTML=point;
+completed.innerHTML=complete;
+uncompleted.innerHTML=uncomplete;
 
 function RenderChapter(){
     
@@ -21,16 +31,36 @@ function RenderChapter(){
     li.innerHTML= input.value;
     if (input.value != 0){
       const del = document.createElement('button');
+      const done= document.createElement('button');
       del.innerHTML="❌"
+      done.innerHTML= "✅"
     
-      list.appendChild(li)
-      li.appendChild(del)
+      list.appendChild(li);
+      li.appendChild(del);
+      li.append(done);
       input.value ='';
+
+    
+    
       
 
       del.addEventListener('click', function() {
           li.remove();
-        });
+          point-=10;
+          points.innerHTML=point;
+          uncomplete+=1;
+          uncompleted.innerHTML=uncomplete;
+
+         
+      });
+      done.addEventListener('click', function() {
+          li.remove();
+          point+=10;
+          console.log(point)
+          points.innerHTML=point;
+          complete+=1;
+          completed.innerHTML=complete;
+      });
     }
 
 }
